8b5f5c57f106c348ae485c99e47741aa345ad6d3
robhhr <robertohhr@pm.me>
2025-07-26 01:57:41 -0700

    print sorted word dictionary

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, get_sorted_dictionary_list
 
 def main():
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
     char_count = get_char_count(book)
+    sorted_dictionary = get_sorted_dictionary_list(char_count)
+
     print(f"{num_words} words found in the document")
     print(f"{char_count}")
+
+    print("count")
+    for i in sorted_dictionary:
+        print(f"{i}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def get_char_count(string):
         else:
             chars[lowered] = 1
     return chars
+
+def get_sorted_dictionary_list(dictionary):
+    dictionary_list = []
+    for i in dictionary:
+        single_dict = dict()
+        lowered = i.lower()
+        single_dict[lowered] = dictionary[i]
+        dictionary_list.append(single_dict)
+    return dictionary_list
+
