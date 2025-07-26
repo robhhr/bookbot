fc2337eb91dbab70257fb1b6fa0b793d7d8e89dd
robhhr <robertohhr@pm.me>
2025-07-26 10:52:29 -0700

    fix sorted list returned

--- stats.py
+++ stats.py
@@ -1,5 +1,10 @@
+def sort_on(items):
+    return items["num"]
+
+
 def get_num_words(string):
     return len(string.split())
+
 
 def get_char_count(string):
     chars = {}
@@ -11,12 +16,15 @@ def get_char_count(string):
             chars[lowered] = 1
     return chars
 
+
 def get_sorted_dictionary_list(dictionary):
     dictionary_list = []
     for i in dictionary:
         single_dict = dict()
         lowered = i.lower()
-        single_dict[lowered] = dictionary[i]
+        single_dict["char"] = lowered
+        single_dict["num"] = dictionary[i]
         dictionary_list.append(single_dict)
-    return dictionary_list
+        dictionary_list.sort(reverse=True, key=sort_on)
 
+    return dictionary_list
