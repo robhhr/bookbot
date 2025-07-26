5d0c0c391b2de2a54858bac7aa24450fcc2868fd
robhhr <robertohhr@pm.me>
2025-07-26 01:37:37 -0700

    add word dic

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def main():
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
+    char_count = get_char_count(book)
     print(f"{num_words} words found in the document")
+    print(f"{char_count}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(string):
     return len(string.split())
 
+def get_char_count(string):
+    chars = {}
+    for c in string:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
